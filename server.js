510e545b3cca6930fe81414441a533c79e8fe076
Pokoh Ufuoma <pokoh.ufuoma@gmail.com>
2024-04-11 12:54:41 +0100

    set up the user schema

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+const uri = process.env.URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  });
 
 //app middlewares
 app.use(morgan("dev"));
