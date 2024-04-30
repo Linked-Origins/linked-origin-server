f75e1e613c610dc5ece5c775a0038d8bfee1cc8f
Pokoh Ufuoma <pokoh.ufuoma@gmail.com>
2024-04-30 17:14:58 +0100

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -171,10 +171,9 @@ exports.registerUser = catchAsync(async (req, res, next) => {
 
 exports.getProfile = catchAsync(async (req, res, next) => {
   const userId = req.params.id;
-  console.log(userId)
+  
   if (userId) {
     const profile = await Users.findOne({ userId: userId });
-
     if (profile) {
       return res.status(200).json({
         message: "success",
